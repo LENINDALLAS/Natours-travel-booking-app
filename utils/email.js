3f9fea7c59d1491571823531deecd1cc0993a6b6
LENINDALLAS <lenindallas.ld@gmail.com>
2022-06-30 08:42:20 +0530

    adding class for sending mail

--- utils/email.js
+++ utils/email.js
@@ -1,29 +1,79 @@
 const nodemailer = require('nodemailer');
+const pug = require('pug');
+const { convert } = require('html-to-text');
 
-const { SMTP_HOST, SMTP_PORT, SMTP_USER, SMTP_PASSWORD } = process.env ;
+const { SMTP_HOST, SMTP_PORT, SMTP_USER, SMTP_PASSWORD, EMAIL_FROM, NODE_ENV } = process.env;
 
-const sendEmail = async options => {
-//     console.log({
-//         SMTP_USER, SMTP_HOST, SMTP_PORT, SMTP_PASSWORD
-// });
-    const transporter = nodemailer.createTransport({
-        host: SMTP_HOST,
-        port: SMTP_PORT,
-        auth: {
-            user: SMTP_USER,
-            pass: SMTP_PASSWORD
+module.exports = class {
+    constructor(user, url) {
+        this.to = user.to;
+        this.firstName = user.name.split(' ')[0];
+        this.url = url;
+        this.from = `Lenin Dallas L <${EMAIL_FROM}>`
+    }
+
+    newTransport() {
+        if (NODE_ENV === 'production') {
+            //sendgrid
+            return 1
         }
-    });
 
-    const mailOptions = {
-        from: "Lenin Dallas L <lenindallas.ld@gmail.com>",
-        to: options.email,
-        subject: options.subject,
-        text:options.message,
-        //html 
-    };
+        return nodemailer.createTransport({
+            host: SMTP_HOST,
+            port: SMTP_PORT,
+            auth: {
+                user: SMTP_USER,
+                pass: SMTP_PASSWORD
+            }
+        })
 
-    await transporter.sendMail(mailOptions);
-};
+    }
 
-module.exports = sendEmail;
+   async send(template, subject) {
+        //prepare html template
+        const html = pug.render(`${__dirname}/../views/email/${template}.pug`, {
+            firstName: this.firstName,
+            url: this.url,
+            subject
+        });
+
+        // options for mail
+        const mailOptions = {
+            from: this.from,
+            to: this.to,
+            subject: subject,
+            text: convert(html),
+            html
+        };
+
+        await this.newTransport().sendMail(mailOptions);
+    }
+
+    async sendWelcome() {
+       await this.send('welcome', 'Welcome to Natours Family...');
+    }
+}
+
+// const sendEmail = async options => {
+//     //     console.log({
+//     //         SMTP_USER, SMTP_HOST, SMTP_PORT, SMTP_PASSWORD
+//     // });
+//     const transporter = nodemailer.createTransport({
+//         host: SMTP_HOST,
+//         port: SMTP_PORT,
+//         auth: {
+//             user: SMTP_USER,
+//             pass: SMTP_PASSWORD
+//         }
+//     });
+
+//     const mailOptions = {
+//         from: "Lenin Dallas L <lenindallas.ld@gmail.com>",
+//         to: options.email,
+//         subject: options.subject,
+//         text: options.message,
+//         //html 
+//     };
+
+//     await transporter.sendMail(mailOptions);
+// };
