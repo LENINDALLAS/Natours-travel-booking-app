6afe757b50f78ea5bcc194bb93e6794cb0ce9248
LENINDALLAS <lenindallas.ld@gmail.com>
2022-07-08 08:53:08 +0530

    switching to sendgrid service for production email notification

--- utils/email.js
+++ utils/email.js
@@ -1,8 +1,11 @@
 const nodemailer = require('nodemailer');
 const pug = require('pug');
 const { convert } = require('html-to-text');
+const sgMail = require('@sendgrid/mail')
 
-const { SMTP_HOST, SMTP_PORT, SMTP_USER, SMTP_PASSWORD, EMAIL_FROM, NODE_ENV } = process.env;
+const { SMTP_HOST, SMTP_PORT, SMTP_USER, SMTP_PASSWORD, EMAIL_FROM, NODE_ENV, SENDGRID_KEY } = process.env;
+
+sgMail.setApiKey(SENDGRID_KEY);
 
 module.exports = class {
     constructor(user, url) {
@@ -12,11 +15,17 @@ module.exports = class {
         this.from = `Lenin Dallas L <${EMAIL_FROM}>`
     }
 
-    newTransport() {
-        if (NODE_ENV === 'production') {
-            //sendgrid
-            return 1
-        }
+    newTransport() {        
+        // if (NODE_ENV.trim() === 'production') {
+        //     console.log('sendgrid', SENDGRID_NAME.length, SENDGRID_KEY.length)
+        //     return nodemailer.createTransport({
+        //         service: 'SendGrid',
+        //         auth: {
+        //             user: SENDGRID_NAME,
+        //             pass: SENDGRID_KEY
+        //         }
+        //     })
+        // }
 
         return nodemailer.createTransport({
             host: SMTP_HOST,
@@ -46,8 +55,11 @@ module.exports = class {
             text: convert(html),
             html
         };
-
+       if (NODE_ENV.trim() === 'production') {
+          await sgMail.send(mailOptions)
+       } else {
         await this.newTransport().sendMail(mailOptions);
+       }
     }
 
     async sendWelcome() {
